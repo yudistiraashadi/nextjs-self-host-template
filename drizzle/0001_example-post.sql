bf3c2c1887b0a979c4e7346bbdb7e1396f4c43e9
Yudistira Ashadi <distra96@yahoo.com>
2025-05-04 20:50:51 +0700

    chore: remove EXAMPLE_posts table and associated metadata from the database schema

--- drizzle/0001_example-post.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "EXAMPLE_posts" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"title" text NOT NULL,
-	"content" text NOT NULL,
-	"image" text,
-	"is_protected" boolean DEFAULT false NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
-);
